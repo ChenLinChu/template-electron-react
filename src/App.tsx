450e4f0f45ba085fb330d93b6f3ba156e1c9a0a5
ChenLin Chu <chenlinchu@ChenLins-Macbook.local>
2022-03-26 10:26:12 +0800

    [更新] 調整 lint 設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import logo from "./logo.svg";
-import "./App.scss";
+import logo from './logo.svg';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="test">
-        <div className="text">fdskglj</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="app-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
